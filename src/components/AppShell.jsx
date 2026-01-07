f3c6643e1022e1b1ed520cdb862710801750b9a9
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2026-01-07 17:14:39 +0700

    Update AppShell.jsx

--- src/components/AppShell.jsx
+++ src/components/AppShell.jsx
@@ -1,13 +1,20 @@
+import { useLocation } from "react-router-dom";
+
 import TopNavbar from "./TopNavbar";
 import BottomNavMobile from "./BottomNavMobile";
 import LoadingOverlay from "./LoadingOverlay";
 
 export default function AppShell({ children }) {
+  const { pathname } = useLocation();
+
+  // hide bottom nav on player route
+  const isPlayer = pathname.startsWith("/player");
+
   return (
     <div className="pansa-shell">
       <TopNavbar />
       {children}
-      <BottomNavMobile />
+      {!isPlayer && <BottomNavMobile />}
       <LoadingOverlay />
     </div>
   );
